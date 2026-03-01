a3cfc3a1ad4665f2e6d3b8c799d2ff45e52bbe4d
Francesco Mondello <faster3ck@gmail.com>
2026-03-01 19:05:40 +0100

    Version 0.15.2.1 Stable - 'MacPorts Fix' + Updated Turkish and Italian translations + Various bugfixes

--- src/globals.h
+++ src/globals.h
@@ -41,8 +41,8 @@ using namespace std;
 namespace globals
 {
     const QString PROGRAM_NAME = "Converseen";
-    const int CURRENT_INTERNAL_VERSION = 1620;
-    const QString VERSION = "0.15.2.0";
+    const int CURRENT_INTERNAL_VERSION = 1621;
+    const QString VERSION = "0.15.2.1";
 
     class Globals
     {
